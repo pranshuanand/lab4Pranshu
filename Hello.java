7895adac5e140e8ab337c5b11a6ad0031d4248ef
Pranshu Anand <pranshuanand@LABS.SCE.CARLETON.CA>
2018-10-05 21:34:40 -0400

    Check

--- Hello.java
+++ Hello.java
@@ -1,7 +1,8 @@
 
-public class Hello {
+public class Hello 
+{
 
-	public static main(String[] args) 
+	public static voidmain(String[] args) 
 	{
 		Hello hello = new Hello();
 		hello.methodA();
@@ -9,9 +10,16 @@ public class Hello {
 		hello.methodC();
 	}
 
-	public void methodA(void ) { }
+	public void methodA(void ) 
+	{
+	System.out.println("Hello Tanvir from Pranshu"); 
+	}
 
-	public void methodb(void) { }
+	public void methodB() 
+	{ 
+	}
 
-	public void methodC(void) { }
+	public void methodC() 
+	{
+	}
 }
